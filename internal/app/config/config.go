fb552802606af5cfbb389bbb354237b203e38072
Eduard Tungatarov <eduard.tungatarov@yandex.ru>
2025-04-24 23:55:15 +0500

    iter5 is done

--- internal/app/config/config.go
+++ internal/app/config/config.go
@@ -1,6 +1,9 @@
 package config
 
-import "flag"
+import (
+	"flag"
+	"os"
+)
 
 type Config struct {
 	ServerHostPort string
@@ -11,6 +14,16 @@ func LoadFromFlag() Config  {
 	a := flag.String("a", "localhost:8080", "отвечает за адрес запуска HTTP-сервера")
 	b := flag.String("b", "http://localhost:8080", "отвечает за базовый адрес результирующего сокращённого URL")
 	flag.Parse()
+
+	aEnv, ok := os.LookupEnv("SERVER_ADDRESS")
+	if ok {
+		*a = aEnv
+	}
+
+	bEnv, ok := os.LookupEnv("BASE_URL")
+	if ok {
+		*b = bEnv
+	}
 
 	return Config{
 		ServerHostPort: *a,
