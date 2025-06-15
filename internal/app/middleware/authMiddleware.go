48c8b22c62cf81368432a53b98b069f7897482f5
Eduard Tungatarov <eduard.tungatarov@yandex.ru>
2025-06-15 15:49:42 +0500

    iter14 - add crypt and decrypt

--- internal/app/middleware/authMiddleware.go
+++ internal/app/middleware/authMiddleware.go
@@ -2,11 +2,20 @@ package middleware
 
 import (
 	"context"
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/sha256"
 	"errors"
 	"github.com/eduardtungatarov/shortener/internal/app"
 	"github.com/google/uuid"
 	"net/http"
 )
+
+var key [32]byte
+
+func Init()  {
+	key = sha256.Sum256([]byte("fvbrqaq!33"))
+}
 
 func (m *Middleware) WithAuth(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(res http.ResponseWriter, req *http.Request) {
@@ -20,7 +29,11 @@ func (m *Middleware) WithAuth(next http.Handler) http.Handler {
 				res.WriteHeader(http.StatusInternalServerError)
 				return
 			}
-			userIDCrypted = generateUserID()
+			userIDCrypted, err = generateUserID()
+			if err != nil {
+				res.WriteHeader(http.StatusInternalServerError)
+				return
+			}
 			setCookie = true
 		} else {
 			userIDCrypted = c.Value
@@ -48,10 +61,43 @@ func (m *Middleware) WithAuth(next http.Handler) http.Handler {
 	})
 }
 
-func generateUserID() string {
-	return uuid.NewString()
+func generateUserID() (string, error) {
+	userId := []byte(uuid.NewString())
+
+	aesblock, err := aes.NewCipher(key[:])
+	if err != nil {
+		return "", err
+	}
+
+	aesgcm, err := cipher.NewGCM(aesblock)
+	if err != nil {
+		return "", err
+	}
+
+	nonce := key[len(key)-aesgcm.NonceSize():]
+
+	encrypted := aesgcm.Seal(nil, nonce, userId, nil)
+
+	return string(encrypted), nil
 }
 
 func getUserID(userIDCrypted string) (string, error) {
-	return userIDCrypted, nil
+	aesblock, err := aes.NewCipher(key[:])
+	if err != nil {
+		return "", err
+	}
+
+	aesgcm, err := cipher.NewGCM(aesblock)
+	if err != nil {
+		return "", err
+	}
+
+	nonce := key[len(key)-aesgcm.NonceSize():]
+
+	decrypted, err := aesgcm.Open(nil, nonce, []byte(userIDCrypted), nil) // расшифровываем
+	if err != nil {
+		return "", err
+	}
+
+	return string(decrypted), nil
 }
