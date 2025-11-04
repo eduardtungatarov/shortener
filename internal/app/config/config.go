10fd79cfe54286325d044c7e49d75f37c9e226c9
Eduard Tungatarov <eduard.tungatarov@yandex.ru>
2025-11-04 22:05:37 +0500

    iter24

--- internal/app/config/config.go
+++ internal/app/config/config.go
@@ -24,6 +24,8 @@ const (
 	DefaultFileStoragePath = "/tmp/short-url-db.json"
 	// DefaultDatabaseDSN строка с адресом подключения к БД.
 	DefaultDatabaseDSN = ""
+	// DefaultEnableHTTPS по дефолту HTTPS отключен.
+	DefaultEnableHTTPS = false
 )
 
 // UserIDKeyName имя ключа для поиска в контексте userID авторизованного пользователя сервиса.
@@ -32,6 +34,7 @@ const UserIDKeyName UserIDKey = "userId"
 // Config настройки сервиса.
 type Config struct {
 	ServerHostPort  string // адрес запуска HTTP сервера
+	EnableHTTPS     bool   // включить https
 	BaseURL         string // базовый адрес результирующего сокращённого URL
 	FileStoragePath string // путь до файла, куда сохраняются все сокращенные URL
 	Database               // настройки бд
@@ -50,6 +53,7 @@ func LoadFromFlag() Config {
 	flagBaseURL := flag.String("b", DefaultBaseURL, "отвечает за базовый адрес результирующего сокращённого URL")
 	flagFileStoragePath := flag.String("f", DefaultFileStoragePath, "путь до файла, куда сохраняются все сокращенные URL")
 	databaseDSN := flag.String("d", DefaultDatabaseDSN, "строка с адресом подключения к БД")
+	enableHTTPS := flag.Bool("s", DefaultEnableHTTPS, "запуск сервера по защищенному протоколу HTTPS")
 	flag.Parse()
 
 	aEnv, ok := os.LookupEnv("SERVER_ADDRESS")
@@ -72,8 +76,14 @@ func LoadFromFlag() Config {
 		*databaseDSN = dEnv
 	}
 
+	sEnv, ok := os.LookupEnv("ENABLE_HTTPS")
+	if ok && sEnv == "true" {
+		*enableHTTPS = true
+	}
+
 	return Config{
 		ServerHostPort:  *flagServer,
+		EnableHTTPS:     *enableHTTPS,
 		BaseURL:         *flagBaseURL,
 		FileStoragePath: *flagFileStoragePath,
 		Database: Database{
